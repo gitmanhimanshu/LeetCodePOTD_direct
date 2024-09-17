e09bc1bf1bf31f05af3332c02c64fcbd979b0b9c
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2024-09-17 18:25:20 +0530

    Time: 3 ms (71.33%), Space: 43.2 MB (26.36%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,28 +1,24 @@
 class Solution {
     public int longestValidParentheses(String s) {
         Stack<Integer> him=new Stack<>();
-        List<Integer> him1=new ArrayList<>();
-        
+      //  List<Integer> him1=new ArrayList<>();
+        him.push(-1);
+     int ans=0;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='('){
+               //  System.out.println(i);
                 him.push(i);
-            }else if(s.charAt(i)==')'){
-                if(!him.isEmpty()&&s.charAt(him.peek())=='('){
-                    him.pop();
-                }else{
-                    him.add(i);
+            }else{
+                him.pop();
+                if(him.isEmpty()){
+                    him.push(i);
                 }
+                ans=Math.max(ans,i-him.peek());
             }
-        }
-       him1.add(0,s.length());
-        while(!him.isEmpty()){
-            him1.add(0,him.pop());
-        }
-        him1.add(0,-1);
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<him1.size()-1;i++){
-            max=max>(him1.get(i+1)-him1.get(i)-1)?max:(him1.get(i+1)-him1.get(i)-1);
-        }
-        return max;
+            }
+        
+
+  
+        return ans;
     }
 }
