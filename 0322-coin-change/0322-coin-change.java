0e23b4464a3d0e29cf466b670307b17030675bb6
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2024-09-25 17:13:59 +0530

    Time: 29 ms (25.44%), Space: 45.6 MB (11.93%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,32 @@
+class Solution {
+    int dp[][];
+    public int coinChange(int[] coins, int amount) {
+        dp=new int[coins.length][amount+1];
+        for(int a[]:dp){
+            Arrays.fill(a,-1);
+        }
+    int a=solve(coins,amount,coins.length-1);
+    if(a==(int )(1e9)){
+        return -1;
+    }
+    return a;
+    }
+    int solve(int a[],int m,int i){
+        if(i==0){
+            if(m%a[i]==0){
+                return m/a[i];
+            }else{
+                return (int)(1e9);
+            }}
+            if(dp[i][m]!=-1){
+                return dp[i][m];
+            }
+            int nt=0+solve(a,m,i-1);
+            int t=Integer.MAX_VALUE;
+            if(a[i]<=m){
+                t=1+solve(a,m-a[i],i);
+            }
+            return dp[i][m]= Math.min(t,nt);
+        
+    }
+}
