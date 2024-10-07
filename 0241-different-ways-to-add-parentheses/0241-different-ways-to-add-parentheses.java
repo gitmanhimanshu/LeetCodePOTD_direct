1487274433fca423626a666abc6a9a7866a28cc4
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2024-10-08 05:08:47 +0530

    Time: 1 ms (99.74%), Space: 42.9 MB (30.07%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,21 +1,28 @@
 class Solution {
     public List<Integer> diffWaysToCompute(String expression) {
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < expression.length(); i++) {
-            char c = expression.charAt(i);
-            if (c == '+' || c == '-' || c == '*') {
-                List<Integer> left = diffWaysToCompute(expression.substring(0, i));
-                List<Integer> right = diffWaysToCompute(expression.substring(i + 1));
-                for (int l : left) {
-                    for (int r : right) {
-                        if (c == '+') result.add(l + r);
-                        else if (c == '-') result.add(l - r);
-                        else result.add(l * r);
+        List<Integer> him=new ArrayList<>();
+        for(int i=0;i<expression.length();i++){
+            if(expression.charAt(i)=='+'||expression.charAt(i)=='-'||expression.charAt(i)=='*'){
+                List<Integer> l=diffWaysToCompute(expression.substring(0,i));
+                 List<Integer> r=diffWaysToCompute(expression.substring(i+1));
+                for(Integer j:l){
+                    for(Integer k:r){
+                        if(expression.charAt(i)=='+'){
+                            him.add(j+k);
+                        }
+                        if(expression.charAt(i)=='-'){
+                            him.add(j-k);
+                        }
+                         if(expression.charAt(i)=='*'){
+                            him.add(j*k);
+                        }
                     }
                 }
             }
         }
-        if (result.isEmpty()) result.add(Integer.parseInt(expression));
-        return result;
+        if(him.isEmpty()){
+            him.add(Integer.parseInt(expression));
+        }
+        return him;
     }
 }
