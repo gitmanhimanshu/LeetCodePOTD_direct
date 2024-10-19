c0bf10142c21f2b1809335f3b3d38c401d46a502
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2024-10-19 07:33:38 +0530

    Time: 115 ms (21.01%), Space: 49.1 MB (41.83%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.java
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.java
@@ -1,30 +1,40 @@
 class Solution {
     String dp[];
     public char findKthBit(int n, int k) {
-        dp=new String[n+1];
-        Arrays.fill(dp,"");
-        String s=find(n);
-        //System.out.println(s);
-for(int i=0;i<s.length();i++){
+//         dp=new String[n+1];
+//         Arrays.fill(dp,"");
+//         String s=find(n);
+//         //System.out.println(s);
+// for(int i=0;i<s.length();i++){
+//     if(i+1==k){
+//         return s.charAt(i);
+  
+//     }
+       
+       String a="0";
+       for(int i=1;i<n;i++){
+        String c=a+'1'+invert(a);
+        a=c;
+       }
+ for(int i=0;i<a.length();i++){
     if(i+1==k){
-        return s.charAt(i);
+        return a.charAt(i);
   
     }
-       
 }
    return ' ';
     }
-    String find(int n){
-        if(n==1){
-            return "0";
-        }
-        if(dp[n]!=""){
-            return dp[n];
-        }
-        String s=find(n-1);
-        return dp[n]=s+"1"+invert(s);
+    // String find(int n){
+    //     if(n==1){
+    //         return "0";
+    //     }
+    //     if(dp[n]!=""){
+    //         return dp[n];
+    //     }
+    //     String s=find(n-1);
+    //     return dp[n]=s+"1"+invert(s);
  
-    }
+    // }
     String invert(String s){
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<s.length();i++){
