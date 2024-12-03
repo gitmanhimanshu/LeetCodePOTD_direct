608269d57caaa2e84747dea5383e097c0f84d697
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2024-12-03 17:19:44 +0530

    Time: 3 ms (28.29%), Space: 44 MB (12.21%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,19 +1,32 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        // Set<Integer> him=new HashSet<>();
-        // for(int i:arr){
-        //     him.add(i);
-        // }
+        Set<Integer> him=new HashSet<>();
+        int z=0;
+        for(int i:arr){
+            him.add(i);
+            if(i==0){
+                z++;
+            }
+        }
         for(int i=0;i<arr.length;i++){
-            int c=arr[i]*2;
-            for(int j=0;j<arr.length;j++){
-                if(i==j){
-                    continue;
-                }
-                if(arr[j]==c){
-                    System.out.println(c);
+            if(arr[i]==0){
+                if(z>1){
                     return true;
                 }
+                continue;
+            }
+            int c=arr[i]*2;
+            // for(int j=0;j<arr.length;j++){
+            //     if(i==j){
+            //         continue;
+            //     }
+            //     if(arr[j]==c){
+            //         System.out.println(c);
+            //         return true;
+            //     }
+            // }
+            if(him.contains(c)){
+                return true;
             }
         }
         return false;
