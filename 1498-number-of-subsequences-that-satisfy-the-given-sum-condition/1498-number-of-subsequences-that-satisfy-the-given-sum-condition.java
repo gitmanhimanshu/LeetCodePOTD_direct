f834317757f240672e9b20da309af95f992e1e41
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-06-29 08:02:05 +0530

    Time: 27 ms (99.52%), Space: 55.5 MB (25.24%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,33 +1,57 @@
 class Solution {
-    int mod=1000000007;;
+     int mod=1000000007;;
     public int numSubseq(int[] nums, int target) {
-        Arrays.sort(nums);
-    int i=0;
-    int ans=0;
-    int j=nums.length-1;
-    while(i<=j){
+         Arrays.sort(nums);
+         int p[]=new int[nums.length];
+         p[0]=1;
+         for(int i=1;i<p.length;i++){
+            p[i]=(p[i-1]*2) % mod;;
+         }
+          int i=0;
+          int j=nums.length-1;
+          int ans=0;
+           while(i<=j){
+            if(nums[i]+nums[j]<=target){
+                 ans = (ans + (p[j-i])) % mod;
+                  i++;
+            }else{
+                j--;
 
-        if(nums[i]+nums[j]<=target){
-            ans = (ans + power(2, j - i)) % mod;
-            i++;
-        }else{
-            j--;
-        }
-    }
-    return ans%mod;
-    }
-        private int power(int base, int exp) {
-        long result = 1;
-        long b = base;
-
-        while (exp > 0) {
-            if ((exp & 1) == 1) {
-                result = (result * b) % mod;
             }
-            b = (b * b) % mod;
-            exp >>= 1;
-        }
-
-        return (int) result;
     }
+    return ans;}
+
 }
+// class Solution {
+//     int mod=1000000007;;
+//     public int numSubseq(int[] nums, int target) {
+//         Arrays.sort(nums);
+//     int i=0;
+//     int ans=0;
+//     int j=nums.length-1;
+//     while(i<=j){
+
+//         if(nums[i]+nums[j]<=target){
+//             ans = (ans + power(2, j - i)) % mod;
+//             i++;
+//         }else{
+//             j--;
+//         }
+//     }
+//     return ans%mod;
+//     }
+//         private int power(int base, int exp) {
+//         long result = 1;
+//         long b = base;
+
+//         while (exp > 0) {
+//             if ((exp & 1) == 1) {
+//                 result = (result * b) % mod;
+//             }
+//             b = (b * b) % mod;
+//             exp >>= 1;
+//         }
+
+//         return (int) result;
+//     }
+// }
