eb90785fee8dd7004d774a789f04d9a68c579fd5
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-07-11 10:17:49 +0530

    Time: 33 ms (5.05%), Space: 62.6 MB (81.4%) - LeetHub

--- 3439-reschedule-meetings-for-maximum-free-time-i/3439-reschedule-meetings-for-maximum-free-time-i.java
+++ 3439-reschedule-meetings-for-maximum-free-time-i/3439-reschedule-meetings-for-maximum-free-time-i.java
@@ -1,32 +1,26 @@
 class Solution {
     public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
-        List<Integer> him = new ArrayList<>();
-        him.add(startTime[0]);  // Time before first event
-
-        // Time between events
-        for (int i = 1; i < startTime.length; i++) {
-            him.add(startTime[i] - endTime[i - 1]);
+        List<Integer> him=new ArrayList<>();
+        him.add(startTime[0]);
+        for(int i=1;i<startTime.length;i++){
+            him.add(startTime[i]-endTime[i-1]);
         }
-
-        // Time after last event to end
-        him.add(eventTime - endTime[endTime.length - 1]);
-
-        int ans = 0, temp = 0;
-        int window = k + 1;
-        int sum = 0;
-
-        // First window sum
-        for (int i = 0; i < Math.min(window, him.size()); i++) {
-            sum += him.get(i);
+        him.add(eventTime-endTime[endTime.length-1]);
+        int ans=0;
+        int temp=0;
+        int i=0;
+        int j=0;
+        System.out.println(him);
+        while(j<him.size()){
+          while(j<him.size()&&j-i+1<=k+1){
+            temp+=him.get(j);
+            j++;
+          }
+          ans=Math.max(ans,temp);
+          
+          temp-=him.get(i);
+          i++;
         }
-        ans = sum;
-
-        // Sliding window
-        for (int i = window; i < him.size(); i++) {
-            sum += him.get(i) - him.get(i - window);
-            ans = Math.max(ans, sum);
-        }
-
         return ans;
     }
 }
