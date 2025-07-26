39c2602b561149a2355dd27b9fd1523fab350ee6
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-07-26 16:51:08 +0530

    Time: 52 ms (15.03%), Space: 45.9 MB (40.69%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.java
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.java
@@ -1,14 +1,18 @@
 class Solution {
     public String makeFancyString(String s) {
-     StringBuilder him=new StringBuilder();
-        for(char i:s.toCharArray()){
-            if(him.length()>=2&&him.charAt(him.length()-2)==i&&him.charAt(him.length()-1)==i){
+        StringBuilder ans=new StringBuilder();
+        if (s.length()<3){
+            return s;
+        }
+        ans.append(s.charAt(0));
+        ans.append(s.charAt(1));
+        for(int i=2;i<s.length();i++){
+            if(s.charAt(i)==ans.charAt(ans.length()-1)&&s.charAt(i)==ans.charAt(ans.length()-2)){
                 continue;
             }else{
-                him.append(i);
-                
+                ans.append(s.charAt(i));
             }
         }
-        return him.toString();
+        return ans.toString();
     }
 }
