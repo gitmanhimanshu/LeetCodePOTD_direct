5fc192052587c1033eaa2a5522f137a41d7f8dfb
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-08-03 08:42:36 +0530

    Time: 43 ms (22.54%), Space: 65.6 MB (5.3%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
@@ -1,25 +1,35 @@
 class Solution {
+    int m=0;
+    int dp[][];
     public int countMaxOrSubsets(int[] nums) {
-        int maxOR = 0;
+        int max = 0;
         for (int i : nums) {
-            maxOR |= i;
+            max |= i;
         }
-
-        return find(0, nums, maxOR, 0);
-
+        m=max;
+        dp=new int[nums.length+1][max+1];
+        for(int a[]:dp){
+            Arrays.fill(a,-1);
+        }
+        return find(0,nums,0);
     }
-
-    int find(int i, int nums[], int max, int curr) {
-        if (i >= nums.length) {
-            if (curr == max) {
-                return 1;
-            } else {
-                return 0;
-            }
+    // int find(int i,int []nums,int maxOr){
+    //     if(i==nums.length){
+    //         return maxOr==m?1:0;
+    //     }
+    //     int in=find(i+1,nums,nums[i]|maxOr);
+    //     int ex=find(i+1,nums,maxOr);
+    //     return in+ex;
+    // }
+      int find(int i,int []nums,int maxOr){
+        if(i==nums.length){
+            return maxOr==m?1:0;
         }
-        int t=find(i+1,nums,max,curr|nums[i]);
-        int nt=find(i+1,nums,max,curr);
-        return t+nt;
-
+        if(dp[i][maxOr]!=-1){
+            return dp[i][maxOr];
+        }
+        int in=find(i+1,nums,nums[i]|maxOr);
+        int ex=find(i+1,nums,maxOr);
+        return dp[i][maxOr]= in+ex;
     }
 }
