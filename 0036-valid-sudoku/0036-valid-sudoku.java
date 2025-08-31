94e5f288dc1bef1c95cce9bc9778748a4ca0006b
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-08-31 15:56:25 +0530

    Time: 3 ms (52.4%), Space: 44.5 MB (66.45%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,22 +1,59 @@
 class Solution {
-   public boolean isValidSudoku(char[][] board) {
-        int[] row = new int[9];
-        int[] col = new int[9];
-        int[][] rec = new int[3][3];
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                if(board[i][j] == '.') continue;
-                int recI = i / 3;
-                int recJ = j / 3;
-                int c = 1 << (board[i][j] - '0');
-                if((row[i] & c) > 0) return false;
-                if((col[j] & c) > 0) return false;
-                if((rec[recI][recJ] & c) > 0) return false;
-                row[i] |= c;
-                col[j] |= c;
-                rec[recI][recJ] |= c;
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            Set<Character> him = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                if (him.contains(board[i][j])) {
+                    return false;
+                }
+                him.add(board[i][j]);
+            }
+        }
+
+        for (int j = 0; j < 9; j++) {
+            Set<Character> him = new HashSet<>();
+            for (int i = 0; i < 9; i++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                if (him.contains(board[i][j])) {
+                    return false;
+                }
+                him.add(board[i][j]);
+            }
+
+        }
+
+        for (int i = 0; i < 9; i += 3) {
+          int  er = i + 2;
+            for (int j = 0; j < 9; j += 3) {
+           int     ec = j + 2;
+                if (!valid(board, i, j, er, ec)) {
+                    return false;
+                }
             }
         }
         return true;
-}
+    }
+
+    boolean valid(char board[][], int r, int c, int er, int ec) {
+         Set<Character> him = new HashSet<>();
+        for (int i = r; i <= er; i++) {
+           
+            for (int j = c; j <= ec; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                if (him.contains(board[i][j])) {
+                    return false;
+                }
+                him.add(board[i][j]);
+            }
+        }
+        return true;
+    }
+    
 }
