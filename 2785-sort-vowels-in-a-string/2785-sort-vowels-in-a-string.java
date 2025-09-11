325387dfa2e9b2791cc1941a579d10e4e4ea12ce
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-09-11 21:48:29 +0530

    Time: 25 ms (82.32%), Space: 46.7 MB (55.95%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
@@ -1,23 +1,24 @@
 class Solution {
     public String sortVowels(String s) {
-        StringBuilder v=new StringBuilder();
-        for(Character i:s.toCharArray()){
-            if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u'||i=='A'||i=='E'||i=='I'||i=='O'||i=='U'){
+        StringBuilder v = new StringBuilder();
+        for (Character i : s.toCharArray()) {
+            if (i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u' || i == 'A' || i == 'E' || i == 'I' || i == 'O'
+                    || i == 'U') {
                 v.append(i);
             }
         }
-            char c[]=v.toString().toCharArray();
-            Arrays.sort(c);
-            StringBuilder ans=new StringBuilder();
-              int j=0;
-              for(Character i:s.toCharArray()){
-                  if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u'||i=='A'||i=='E'||i=='I'||i=='O'||i=='U'){
-              ans.append(c[j++]);
-              continue;
-            }
-            ans.append(i);
-              }
-              return ans.toString();
-        }
-    }
 
+        char c[] = v.toString().toCharArray();
+        Arrays.sort(c);
+       int j=0;
+       StringBuilder sb=new StringBuilder();
+        for(char i:s.toCharArray()){
+         if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u'||i=='A'||i=='E'||i=='I'||i=='O'||i=='U'){
+            sb.append(c[j++]);
+         }else{
+            sb.append(i);
+         }
+        }
+        return sb.toString();
+    }
+}
