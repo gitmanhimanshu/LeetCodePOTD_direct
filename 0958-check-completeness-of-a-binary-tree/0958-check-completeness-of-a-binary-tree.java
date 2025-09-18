74d5d83d9ca6a6ba5351fe6194ba27444d5a77c3
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-09-18 06:18:11 +0530

    Time: 0 ms (100%), Space: 42.1 MB (56.83%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -13,32 +13,59 @@
  *     }
  * }
  */
-class Solution {
-    public boolean isCompleteTree(TreeNode root) {
-     Queue<TreeNode> him=new LinkedList<>();
- boolean isGot=false;
 
-     him.add(root);
-     while(!him.isEmpty()){
-        int n=him.size();
+
+// class Solution {
+//     public boolean isCompleteTree(TreeNode root) {
+//      Queue<TreeNode> him=new LinkedList<>();
+//  boolean isGot=false;
+
+//      him.add(root);
+//      while(!him.isEmpty()){
+//         int n=him.size();
         
-        while(n>0){
-            n--;
-            TreeNode p=him.poll();
-            if(p==null){
-                isGot=true;
-            }else{
-                if(isGot){
-                    return false;
-                }
-                him.add(p.left);
-                him.add(p.right);
-            }
+//         while(n>0){
+//             n--;
+//             TreeNode p=him.poll();
+//             if(p==null){
+//                 isGot=true;
+//             }else{
+//                 if(isGot){
+//                     return false;
+//                 }
+//                 him.add(p.left);
+//                 him.add(p.right);
+//             }
 
 
+//         }
+
+//      }   
+//      return true;
+//     }
+// }
+class Solution {
+    int count(TreeNode root){
+        if(root==null){
+            return 0;
         }
-
-     }   
-     return true;
+        int l=count(root.left);
+        int r=count(root.right);
+        return 1+l+r;
+    }
+    public boolean isCompleteTree(TreeNode root) {
+        int total=count(root);
+        return isComplete(root,1,total);        
+    }
+    boolean isComplete(TreeNode root,int ind,int total){
+        if(root==null){
+            return true;
+        }
+        if(ind>total){
+            return false;
+        }
+        int l=2*ind;
+        int r=2*ind+1;
+        return isComplete(root.left,l,total)&&isComplete(root.right,r,total);
     }
 }
