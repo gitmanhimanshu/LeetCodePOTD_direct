00c7ba8a36208f1eac252d748d4c497916f080e5
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-09-22 20:04:48 +0530

    Time: 40 ms (6.8%), Space: 46.1 MB (79.46%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,21 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int ans[]=new int[nums.length];
-        // intially all elements are zero by default .. ans=[0,0,0,0,0];
-        int k=0; // take k variable for storinng nonzero count in ans array
-        for(int i=0;i<nums.length;i++){
-            // if(elements is non zero then add in to ans array
-            if(nums[i]!=0){
-                ans[k]=nums[i]; // we store in ans kth position beacuase for only nonzero element
-                k++;                    // increment k if nonzero meet 
-            }
+      
+       int j=0;
+       while(j<nums.length){
+        int num=nums[j];
+        int c=j-1;
+        nums[j]=0;
+        while(c>=0&&nums[c]==0){
+            
+            c--;
+            
         }
-// after loop ans=[1,3,12,0,0,0] beacause we adding nonzero only in ans variable
-  for(int i=0;i<ans.length;i++){
-    nums[i]=ans[i];   // copy all element from final array to initial array
-  }
-
-//  for intput [2,4,0,0,0,7,0,9,0,0,0,0] 
-//  answer should be [2,4,7,9,0,0,0,0,0,0,0...]
+        nums[c+1]=num;
+       
+       
+        j++;
+       } 
+       
     }
 }
