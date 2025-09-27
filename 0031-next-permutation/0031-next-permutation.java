672c4d08f6e8316cafeeb2b45d819516af522173
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-09-27 20:26:08 +0530

    Time: 0 ms (100%), Space: 43.1 MB (57.53%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -2,39 +2,40 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int ind=-1;
         for(int i=nums.length-2;i>=0;i--){
-        if(nums[i]<nums[i+1]){
-            ind=i;
-            break;
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;
+            }
         }
-    }
-        if(ind==-1){
-          int s=0,e=nums.length-1;
-    while(s<e){
-        int c=nums[s];
-        nums[s]=nums[e];
-        nums[e]=c;
-        s++;
-        e--;
-    }
-    
-            return;
+        if (ind==-1){
+            int s=0, e=nums.length-1;
+            while(s<e){
+                int c=nums[s];
+                nums[s]=nums[e];
+                nums[e]=c;
+                s++;
+                e--;
+            }
+                 return;       
         }
-  for(int i=nums.length-1;i>ind;i--){
-      if(nums[ind]<nums[i]){
-          int c=nums[ind];
-          nums[ind]=nums[i];
-          nums[i]=c;
-          break;
-      }
-  }
-    int s=ind+1,e=nums.length-1;
-    while(s<e){
-        int c=nums[s];
-        nums[s]=nums[e];
-        nums[e]=c;
-        s++;
-        e--;
-    }
-    
+        for(int i=nums.length-1;i>ind;i--){
+            if(nums[ind]<nums[i]){
+                int c=nums[ind];
+                nums[ind]=nums[i];
+                nums[i]=c;break;
+            }
+
+        }
+        int s=ind+1;
+        int e=nums.length-1;
+        while(s<e){
+            int c=nums[s];
+            nums[s]=nums[e];
+            nums[e]=c;
+            s++;
+            e--;
+        }
+
+
     }
 }
