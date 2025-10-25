494db26f8a36ea0215ccae0bc7db5cf4d801e96b
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-10-25 10:19:33 +0530

    Time: 1 ms (91.05%), Space: 44.9 MB (74.1%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -4,29 +4,29 @@ class Solution {
         int right = 1;
 
         int ans = Integer.MIN_VALUE;
-        // for(int i=0;i<nums.length;i++){
-        // left=(left==0?1:left)*nums[i];
-        // right=(right==0?1:right)*nums[nums.length-i-1];
-        //     ans=Math.max(ans,Math.max(left,right));
-        // }
-        for (int i : nums) {
-            if (left == 0) {
-                left = 1;
+        for(int i=0;i<nums.length;i++){
+        left=(left==0?1:left)*nums[i];
+        right=(right==0?1:right)*nums[nums.length-i-1];
+            ans=Math.max(ans,Math.max(left,right));
+        }
+        // for (int i : nums) {
+        //     if (left == 0) {
+        //         left = 1;
 
-            } 
-            left = left * i;
+        //     } 
+        //     left = left * i;
             
-            ans = Math.max(ans, left);
-        }
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (right == 0) {
-                right = 1;
+        //     ans = Math.max(ans, left);
+        // }
+        // for (int i = nums.length - 1; i >= 0; i--) {
+        //     if (right == 0) {
+        //         right = 1;
 
-            }
-            right=right*nums[i];
+        //     }
+        //     right=right*nums[i];
 
-             ans = Math.max(ans, right);
-        }
+        //      ans = Math.max(ans, right);
+        // }
         return ans;
     }
 }
