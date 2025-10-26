d9c3c3e49015d1df757e8bf206932d1564580d86
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-10-26 16:49:06 +0530

    Time: 0 ms (100%), Space: 46.2 MB (15.19%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,16 +1,35 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int s=0,e=nums.length-1;
-        while(s<=e){
-            int m=s+(e-s)/2;
-            if(nums[m]==target){
-                return m;
-            }else if(nums[m]>target){
-                e=m-1;
-            }else{
-                s=m+1;
-            }
+        return search(nums,0,nums.length-1,target);
+    }
+    int search(int nums[],int s,int e,int t){
+        if(s>e){
+            return -1;
         }
-        return -1;
+        int m=s+(e-s)/2;
+        if(nums[m]==t){
+            return m;
+        }
+        if(nums[m]>t){
+            return search(nums,s,m-1,t);
+        }
+        return search(nums,m+1,e,t);
     }
 }
+
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int s=0,e=nums.length-1;
+//         while(s<=e){
+//             int m=s+(e-s)/2;
+//             if(nums[m]==target){
+//                 return m;
+//             }else if(nums[m]>target){
+//                 e=m-1;
+//             }else{
+//                 s=m+1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
