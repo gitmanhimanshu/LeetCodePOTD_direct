6444a71c1887406f96afb299377b6298661e7e5d
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-10-27 19:55:38 +0530

    Time: 0 ms (100%), Space: 46.1 MB (50.14%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,33 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int ans[]=new int [2];
-        ans[0]=leftbin(nums,target);
-        ans[1]=rightbin(nums,target);
-        return ans;
-    }
-    int leftbin(int a[],int target){
-        int s=0;
-        int e=a.length-1;
-        int ans=-1;
-        while(s<=e){
-            int mid=(s+e)/2;
-            if(a[mid]==target){
-                ans=mid;
-                e=mid-1;
+        int left = -1;
+        int right = -1;
+        int s = 0, r = nums.length - 1;
+        while (s <= r) {
+            int m = s + (r - s) / 2;
+            if (nums[m] == target) {
+                left = m;
+                r = m - 1;
+            } else if (nums[m] > target) {
+                r = m - 1;
+            } else {
+                s = m + 1;
             }
-            else if(a[mid]>target){
-                e=mid-1;
-            }else{
-                s=mid+1;
-            }
-            
         }
-        return ans;
-    }
-    int rightbin(int a[],int target){
-        int s=0;
-        int e=a.length-1;
-        int ans=-1;
-        while(s<=e){
-            int mid=(s+e)/2;
-            if(a[mid]==target){
-                ans=mid;
-                s=mid+1;
+         s = 0;
+         r = nums.length - 1;
+        while (s <= r) {
+            int m = s + (r - s) / 2;
+            if (nums[m] == target) {
+                right = m;
+                s = m + 1;
+            } else if (nums[m] > target) {
+                r = m - 1;
+            } else {
+                s = m + 1;
             }
-            else if(a[mid]>target){
-                e=mid-1;
-            }else{
-                s=mid+1;
-            }
-            
         }
-        return ans;
+        return new int[]{left,right};
     }
+
 }
