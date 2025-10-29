7b584af91b9519bbcab610833b647b27a9fd1a8e
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-10-29 20:24:01 +0530

    Time: 0 ms (100%), Space: 42.2 MB (54.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,26 +1,29 @@
 class Solution {
+
+
     int pivot(int a[]){
-        int l=0;
-        int r=a.length-1;
-        while(l<r){
-            int m=(l+r)/2;
-            if(a[m]>a[r]){
-                l=m+1;
+        int s=0;
+        int e=a.length-1;
+        while(s<e){
+            int m=s+(e-s)/2;
+            if(a[m]>a[e]){
+                s=m+1;
             }else{
-                r=m;
+                e=m;
             }
         }
-        return r;
+        return s;
     }
     public int search(int[] nums, int target) {
         int p=pivot(nums);
-      //  System.out.println(p);
-        int e=binsearch(nums,target,0,p-1);
+                int e=binsearch(nums,target,0,p-1);
         if(e!=-1){
             return e;
         }
         return binsearch(nums,target,p,nums.length-1);
-    }
+    }   
+    
+    
     int binsearch(int a[],int ele,int s,int e){
         while(s<=e){
             int mid=(s+e)/2;
