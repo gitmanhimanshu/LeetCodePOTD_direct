c65985e6465774281366e17b37c3bbfd13356fce
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-12-08 21:55:04 +0530

    Time: 2 ms (47.59%), Space: 48.9 MB (26.93%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,27 +1,58 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-    int n=nums1.length+nums2.length;
-        int temp[]=new int[n];
-        int i=0;
-        int j=0;
-        int k=0;
-        while(i<nums1.length&&j<nums2.length){
-            if(nums1[i]<nums2[j]){
-                temp[k++]=nums1[i++];
-            }else{
-                temp[k++]=nums2[j++];
+        int ele1 = -1;
+        int ele2 = -1;
+        int n = nums1.length + nums2.length;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int reach1 = n / 2;
+        int reach2 = n / 2 - 1;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+
+                if (k == reach1) {
+                    ele1 = nums1[i];
+                }
+                if (k == reach2) {
+                    ele2 = nums1[i];
+                }
+                k++;
+                i++;
+            } else {
+                if (k == reach1) {
+                    ele1 = nums2[j];
+                }
+                if (k == reach2) {
+                    ele2 = nums2[j];
+                }
+                k++;
+                j++;
             }
         }
-        while(i<nums1.length){
-        temp[k++]=nums1[i++];
+        while (i < nums1.length) {
+            if (k == reach1) {
+                ele1 = nums1[i];
+            }
+            if (k == reach2) {
+                ele2 = nums1[i];
+            }
+            k++;
+            i++;
         }
-        while(j<nums2.length){
-            temp[k++]=nums2[j++];
+        while (j < nums2.length) {
+            if (k == reach1) {
+                ele1 = nums2[j];
+            }
+            if (k == reach2) {
+                ele2 = nums2[j];
+            }
+            k++;
+            j++;
         }
         if(n%2!=0){
-            return temp[n/2];
+            return ele1;
         }
-        return (temp[n/2-1]+temp[n/2])/2.0;
-
+        return (ele1+ele2)/2.0;
     }
 }
