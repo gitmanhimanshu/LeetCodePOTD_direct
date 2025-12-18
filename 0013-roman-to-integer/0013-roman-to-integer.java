6c0bc39af2593b0fb1c22ae18d9a5eb8913231a5
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-12-18 20:41:14 +0530

    Time: 3 ms (77.54%), Space: 46.4 MB (46.38%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,34 +1,40 @@
 class Solution {
     public int romanToInt(String s) {
-        if(s.length()==0){
-return 0;}
-        int sum=get(s.charAt(0));
-        for(int i=1;i<s.length();i++){
-           
-            if(get(s.charAt(i))>get(s.charAt(i-1))){
-                sum=sum+get(s.charAt(i))-(2*get(s.charAt(i-1)));
-            }
-            else{
-                sum=sum+get(s.charAt(i));
-            }
+        if (s.length() == 0) {
+            return 0;
         }
-        return sum;
+        int ans= getNum(s.charAt(0));
+        for(int i=1;i<s.length();i++){
+            if(getNum(s.charAt(i))>getNum(s.charAt(i-1))){
+                ans+=getNum(s.charAt(i))- (2*getNum(s.charAt(i-1)));
+            }else{
+                ans+=getNum(s.charAt(i));
+            }
+           
+        }
+         return ans;
     }
-    int get(char n){
-        int a=0;
-        switch(n){
-          
-                   case 'I':return 1;
-                   case 'V':return 5;
-                   case 'X':return 10;
-                   case 'L':return 50;
-                   case 'C':return 100;
-                       case 'D':return 500;
-                 case 'M': return 1000;
-            default: return 0;
-                                    
 
-                                
+    int getNum(char n) {
+
+        switch (n) {
+
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
 
         }
     }
