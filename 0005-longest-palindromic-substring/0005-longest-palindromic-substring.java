c9e3702ba991133ab9f3a973b27c38ea6bc48c6a
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-12-20 19:11:21 +0530

    Time: 18 ms (50.88%), Space: 45.4 MB (42.46%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,9 +1,6 @@
 class Solution {
     public String longestPalindrome(String s) {
         String res="";
-        if(s.length()==1){
-            return s;
-        }
         for(int i=0;i<s.length();i++){
             int p=i-1;
             int r=i+1;
@@ -11,34 +8,36 @@ class Solution {
             while(p>=0&&r<s.length()){
                 if(s.charAt(p)==s.charAt(r)){
                     len+=2;
+                    p--;
+                    r++;
                 }else{
                     break;
                 }
-              
-                p--;r++;
+            
             }
-              if(len>res.length()){
-                    int st=i-len/2;
-                    res=s.substring(st,st+len);
-                }
+            if(res.length()<len){
+                int st=i-len/2;
+                res=s.substring(st,st+len);
+            }
         }
-        for(int i=0;i<s.length()-1;i++){
+          for(int i=0;i<s.length();i++){
             int p=i;
             int r=i+1;
             int len=0;
             while(p>=0&&r<s.length()){
                 if(s.charAt(p)==s.charAt(r)){
                     len+=2;
+                    p--;
+                    r++;
                 }else{
                     break;
                 }
-               
-                p--;r++;
+            
             }
-             if(len>res.length()){
-                    int st=i+1-len/2;
-                    res=s.substring(st,st+len);
-                }
+            if(res.length()<len){
+                int st=i+1-len/2;
+                res=s.substring(st,st+len);
+            }
         }
         return res;
     }
