da713004acbbb5462b8d7a8714e632e21085f00a
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-12-25 20:50:30 +0530

    Time: 0 ms (100%), Space: 44.3 MB (52.67%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -8,9 +8,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- 
 class Solution {
-    int size(ListNode head){
+     int size(ListNode head){
      if(head==null){
          return 0;
      }
@@ -23,30 +22,33 @@ class Solution {
      return i;
  }
     public ListNode rotateRight(ListNode head, int k) {
-        if(head==null||head.next==null){
-            return head;
-        }
-        int l=k%size(head);
-        int i=0;
-        while(i<l){
-            ListNode temp=head;
-            ListNode prev=null;
-            while(temp.next!=null){
-                prev=temp;
-                temp=temp.next;
-            }
-            prev.next=null;
-            head=addf(head,temp);
-            i++;
-        }
+    int i=0;
+    if(head==null||head.next==null){
         return head;
     }
-    ListNode addf(ListNode head,ListNode temp){
+    int l=k%size(head);
+    while(i<l){
+        i++;
+        ListNode temp=head;
+        ListNode prev=null;
+        while(temp.next!=null){
+            prev=temp;
+            temp=temp.next;
+        }
+        prev.next=null;
+        head=addhead(head,temp);
+
+    }
+    return head;
+    
+    }
+    ListNode addhead(ListNode head,ListNode temp){
         if(head==null){
-            return head;
+            return temp;
         }
         temp.next=head;
         head=temp;
         return head;
     }
+    
 }
