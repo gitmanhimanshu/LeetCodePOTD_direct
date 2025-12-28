f98ecd90be20076b00522da902d3937feccf10a5
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-12-28 12:05:37 +0530

    Time: 3 ms (38.38%), Space: 45.9 MB (18.4%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,22 @@
 class Solution {
+    List<List<Integer>> ans;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-       List<List<Integer>> ans=new ArrayList<>();
-        fun(0,target,ans,new ArrayList<>(),candidates);
+        ans=new ArrayList<>();
+        solve(candidates,target,0,0,new ArrayList<>());
         return ans;
     }
-    void fun(int ind,int target, List<List<Integer>> ans,List<Integer> him,int a[]){
-        if(ind==a.length){
-            if(target==0){
-                
-                ans.add(new ArrayList<>(him));
-               
-            }
-           return;
+    void  solve(int []arr,int k,int sum,int i,List<Integer> him){
+        if(sum==k){
+            ans.add(new ArrayList<>(him));
+            return;
         }
-            if(target>=a[ind]){
-                him.add(a[ind]);
-                fun(ind,target-a[ind],ans,him,a);
-                him.remove(him.size()-1);
-            }
-            fun(ind+1,target,ans,him,a);
+        if(i>=arr.length){
+            return ;
         }
-    
+        if(k-sum>=arr[i])
+      {  him.add(arr[i]);
+        solve(arr,k,sum+arr[i],i,him);
+        him.remove(him.size()-1);}
+        solve(arr,k,sum,i+1,him);
+    }
 }
