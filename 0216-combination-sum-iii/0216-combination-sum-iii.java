54e0d35856b77b92e92d3f05249a328b149eb251
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-12-28 13:50:51 +0530

    Time: 3 ms (3.14%), Space: 42.7 MB (11.68%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,28 @@
+class Solution {
+    Set<List<Integer>>ans;
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        ans=new HashSet<>();
+        solve(k,n,new ArrayList<>(),0,1);
+        return new ArrayList<>(ans);
+    }
+    void solve(int k,int n,List<Integer> him,int sum,int ind){
+        if(him.size()==k){
+            if(sum==n){
+                ans.add(new ArrayList<>(him));
+                
+            }
+            return;
+        }
+        if(sum>n){
+            return;
+        }
+        for(int i=ind;i<=9;i++){
+            if(sum+i<=n){
+                him.add(i);
+                solve(k,n,him,sum+i,i+1);
+                him.remove(him.size()-1);
+            }
+            solve(k,n,him,sum,i+1);
+        }
+    }
+}
