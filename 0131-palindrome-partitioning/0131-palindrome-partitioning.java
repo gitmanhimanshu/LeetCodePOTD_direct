3ae48854fc3ba38a98ca2d061a972758748d9092
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-12-29 20:19:20 +0530

    Time: 9 ms (26.92%), Space: 65.4 MB (21.42%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,27 +1,33 @@
 class Solution {
+     List<List<String>>  ans;
     public List<List<String>> partition(String s) {
-        List<List<String>> ans=new ArrayList<>();
-        bc(s,0, new ArrayList<>(),ans);
+        ans=new ArrayList<>();
+        solve(s,0,new ArrayList<>());
+
         return ans;
     }
-    boolean aage_aur_peeche_same(String s,int st,int e){
-        while(st<e){
-            if(s.charAt(st)!=s.charAt(e)){
+    boolean same(String s,int se,int e){
+        e=e;
+        while(se<e){
+            if(s.charAt(se)!=s.charAt(e)){
                 return false;
             }
-            st++;e--;
+            se++;
+            e--;
         }
         return true;
     }
-    void bc(String s,int ind,List<String> him,List<List<String>> ans){
-        if(ind==s.length()){
+    void solve(String s,int ind,List<String>him){
+        if(ind>=s.length()){
             ans.add(new ArrayList<>(him));
+            return;
         }
         for(int i=ind;i<s.length();i++){
-            if(aage_aur_peeche_same(s,ind,i)){
+            if(same(s,ind,i)){
                 him.add(s.substring(ind,i+1));
-                bc(s,i+1,him,ans);
+                solve(s,i+1,him);
                 him.remove(him.size()-1);
+
             }
         }
     }
