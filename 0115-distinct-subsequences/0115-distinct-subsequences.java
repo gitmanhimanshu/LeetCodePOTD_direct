39a4964ff2c6fd32e0e5cb650b8eeee6edd08919
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2025-12-31 14:32:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int dp[][]=new int[s.length()][t.length()];
+     return solve(0,s,t,0);   
+    }
+    int solve(int i,String s,String t,int j){
+        
+        if(j>=t.length()){
+            return 1;
+        }
+        if(i>=s.length()){
+            return 0;
+        }
+       int ways=0;
+        if(s.charAt(i)==t.charAt(j)){
+             ways = solve(i+1, s, t, j+1) + solve(i+1, s, t, j);
+        }else{
+            ways=solve(i+1, s, t, j);
+        }
+        return ways;
+    }
+}
