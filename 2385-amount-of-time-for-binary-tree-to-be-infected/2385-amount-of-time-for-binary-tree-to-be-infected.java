474a58467e9594db0aefac0b91358afd7c37da55
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-01-02 06:35:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
@@ -0,0 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<TreeNode, TreeNode> him;
+
+   void parent(TreeNode root) {
+    if (root == null) return;
+
+    if (root.left != null) {
+        him.put(root.left, root);
+        parent(root.left);
+    }
+    if (root.right != null) {
+        him.put(root.right, root);
+        parent(root.right);
+    }
+}
+
+    TreeNode find(int val, TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        TreeNode l = find(val, root.left);
+        TreeNode r = find(val, root.right);
+        if (l != null) {
+            return l;
+        }
+        return r;
+    }
+
+    public int amountOfTime(TreeNode root, int start) {
+        him = new HashMap<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        Set<TreeNode> vis = new HashSet<>();
+        parent(root);
+        TreeNode st = find(start, root);
+        q.add(st);
+        int ans = 0;
+        while (!q.isEmpty()) {
+            int n = q.size();
+            for (int i = 1; i <= n; i++) {
+                TreeNode p = q.poll();
+                vis.add(p);
+                if (p.left != null) {
+                    q.add(p.left);
+                }
+                if (p.right != null) {
+                    q.add(p.right);
+                }
+                TreeNode par = him.get(p);
+                if (par != null && !vis.contains(par)) {
+                   
+                    q.add(par);
+                    
+                }
+            }
+            ans++;
+        }
+        return ans - 1;
+
+    }
+}
