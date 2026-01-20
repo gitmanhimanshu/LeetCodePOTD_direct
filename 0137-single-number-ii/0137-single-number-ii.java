555d69275f4bd9d4e38d80f31dac7aa9b9f5533f
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-01-21 05:01:11 +0530

    Time: 4 ms (61.6%), Space: 45.3 MB (56.89%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,22 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-    int ans=0;
-    for(int i=0;i<=31;i++){
-        int zero=0;
-        int one=0;
-        int t=1;
-        for(int num:nums){
-            if((t<<i&num)==0){
-                zero++;
-            }else{
-                one++;
+        int ans=0;
+        for(int i=0;i<=31;i++){
+            int one=0;
+            
+            for(int num:nums){
+                if((num>>i &1)==1){
+                    one++;
+                }
+            }
+            if(one%3!=0){
+                ans |= (1 << i);
             }
         }
-        if(one%3!=0){
-            ans=ans|(t<<i);
-        }
-        
-    }
-    return ans;
+        return ans;
     }
 }
