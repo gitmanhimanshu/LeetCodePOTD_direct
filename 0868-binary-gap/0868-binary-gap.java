c515489b705bc663f214b965027aad8db0dbddcf
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-02-22 10:57:19 +0530

    Time: 0 ms (100%), Space: 42.2 MB (84.49%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.java
+++ 0868-binary-gap/0868-binary-gap.java
@@ -1,31 +1,51 @@
 class Solution {
-     String decimalToBinary(int n)
-    {
-        int remainder, quotient = n;
-        String binaryNum = "";
-        while (quotient > 0) {
-            remainder = quotient % 2;
-            binaryNum
-                = Integer.toString(remainder) + binaryNum;
-            quotient = quotient / 2;
-        }
-        return binaryNum;
-    }
     public int binaryGap(int n) {
-        String binary = decimalToBinary(n);
-        Map<Integer,Integer> him=new HashMap<>();
+        int last=-1;
+        int pos=0;
         int ans=0;
-        int sum=0;
-        for(int i=0;i<binary.length();i++){
-            int e=binary.charAt(i)-'0';
-            if(e==1){
-                if(him.containsKey(sum)){
-                    ans=Math.max(ans,i-him.get(sum));
+        while(n>0){
+            if((n&1)!=0){
+                if(last!=-1){
+                    ans=Math.max(ans,pos-last);
                 }
-                sum=sum+1;
-                him.put(sum,i);
+                last=pos;
             }
+            n=n>>1;
+            pos++;
         }
         return ans;
     }
 }
+
+
+// class Solution {
+// String decimalToBinary(int n) {
+//     if (n == 0) return "0";
+
+//     StringBuilder sb = new StringBuilder();
+
+//     while (n > 0) {
+//         sb.append(n % 2); 
+//         n = n / 2;        
+//     }
+
+//     return sb.reverse().toString(); 
+// }
+//     public int binaryGap(int n) {
+//         String binary = decimalToBinary(n);
+//         Map<Integer,Integer> him=new HashMap<>();
+//         int ans=0;
+//         int sum=0;
+//         for(int i=0;i<binary.length();i++){
+//             int e=binary.charAt(i)-'0';
+//             if(e==1){
+//                 if(him.containsKey(sum)){
+//                     ans=Math.max(ans,i-him.get(sum));
+//                 }
+//                 sum=sum+1;
+//                 him.put(sum,i);
+//             }
+//         }
+//         return ans;
+//     }
+// }
