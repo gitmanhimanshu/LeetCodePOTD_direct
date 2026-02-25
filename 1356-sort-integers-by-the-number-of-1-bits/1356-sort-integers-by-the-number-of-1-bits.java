b720459fbbd24da46e7713a2a210b9f28b4c2ec1
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-02-25 06:23:40 +0530

    Time: 6 ms (93.7%), Space: 46.8 MB (39.38%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,38 +1,37 @@
 class pair implements Comparable<pair>{
-    int n;
-    int bit;
-    pair(int n,int bit){
-        this.n=n;
-        this.bit=bit;
-    }
-    public int compareTo( pair o){
-int diff=this.bit-o.bit;
-if(diff!=0){
-    return diff;
-}
-return this.n-o.n;
-    }
-}
-class Solution {
-    int find(int n) {
-        int c = 0;
-        while (n > 0) {
-            if (n % 2 == 1) {
-                c++;
-            }
-            n /= 2;
-        }
-        return c;
-    }
-    public int[] sortByBits(int[] arr) {
-        PriorityQueue<pair> him=new PriorityQueue<>();
-        for(int i:arr){
-            him.add(new pair(i,find(i)));
-        }
-        int k=0;
- while(!him.isEmpty()){
-    arr[k++]=him.poll().n;
+int ele;
+int bit;
+ pair(int ele,int bit){
+    this.ele=ele;
+    this.bit=bit;
  }
- return arr;
+ public int compareTo(pair that){
+    int diff= this.bit-that.bit;
+    if(diff!=0){
+        return diff;
+    }
+    return this.ele-that.ele;
+ }
+}
+
+class Solution {
+    int find(int n){
+    int c=0;
+    while(n>0){
+        c+=(n&1);
+        n=n>>1;
+    }
+    return c;
+}
+    public int[] sortByBits(int[] arr) {
+     PriorityQueue<pair> him=new PriorityQueue<>();
+     for (int i:arr){
+        him.add(new pair(i,find(i)));
+     }
+   int i=0;
+   while(!him.isEmpty()){
+    arr[i++]=him.poll().ele;
+   }
+       return arr; 
     }
 }
