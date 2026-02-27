ca5c752f7bd7b7e41560d1c675379b7198156ac4
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-02-27 06:35:42 +0530

    Time: 415 ms (71.5%), Space: 31.7 MB (43.74%) - LeetHub

---
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxDistance(self, position: List[int], m: int) -> int:
+        l=1
+        r=max(position)
+        position.sort()
+        ans=-1
+        while(l<=r):
+            mid=l+(r-l)//2
+            if(self.isPos(position,mid,m)):
+                l=mid+1
+                ans=mid
+            else:
+                r=mid-1
+        return ans
+    def isPos(self,a:List[int],m:int,k:int):
+        last=a[0]
+        k=k-1
+        for i in a:
+            if (i-last >=m):
+                k=k-1
+                last=i
+            if k==0:
+                return True
+        return k==0
+
+        
