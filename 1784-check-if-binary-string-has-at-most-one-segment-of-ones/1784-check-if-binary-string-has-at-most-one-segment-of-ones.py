1c735adce335bdfd9bf5e7802255fedc223ef0b9
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-03-06 06:17:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.py
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.py
@@ -1,6 +1,8 @@
 class Solution:
     def checkOnesSegment(self, s: str) -> bool:
-        return "01" not in s
+    #    return "01" not in s
+        return s.count("01")==0
+
 
 # // class Solution {
 # //     public boolean checkOnesSegment(String s) {
