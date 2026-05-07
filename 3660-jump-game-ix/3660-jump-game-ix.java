bee9c549d34d983cdcd33158a97514fa3d1b91ff
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-05-07 20:37:27 +0530

    Time: 5 ms (87.1%), Space: 197.7 MB (85.48%) - LeetHub

---
+++ 3660-jump-game-ix/3660-jump-game-ix.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] maxValue(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        int[] preMax = new int[n];
+
+        preMax[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            preMax[i] = Math.max(preMax[i - 1], nums[i]);
+        }
+
+        int sufMin = Integer.MAX_VALUE;
+
+
+        for (int i = n - 1; i >= 0; i--) {
+
+      
+            if (preMax[i] > sufMin) {
+                ans[i] = (i + 1 < n) ? ans[i + 1] : preMax[i];
+            } else {
+                ans[i] = preMax[i];
+            }
+
+            sufMin = Math.min(sufMin, nums[i]);
+        }
+
+        return ans;
+    }
+}
