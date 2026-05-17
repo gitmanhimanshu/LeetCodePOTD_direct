289d1129c75164cd34873998c25c19449e26255f
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-05-17 12:20:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,27 +1,32 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        List<List<Integer>> him=new ArrayList<>();
+        Map<Integer,List<Integer>> him=new HashMap<>();
         for(int i=0;i<isConnected.length;i++){
-            him.add(new ArrayList<>());
-            for(int j=0;j<isConnected.length;j++){
-               if(isConnected[i][j]==1) him.get(i).add(j);
-            }
+            him.put(i,new ArrayList<>());
         }
-        int vis[]=new int[isConnected.length];
-        int ans=0;
-        for(int i=0;i<vis.length;i++ ){
-            if(vis[i]==0){
+        for(int i=0;i<isConnected.length;i++){
+            for(int j=0;j<isConnected.length;j++){
+                if(isConnected[i][j]==1){
+                    him.get(i).add(j);
+                }
+            }
+       
+        }
+         int ans=0;
+        int v[]=new int[isConnected.length];
+        for(int i=0;i<v.length;i++){
+            if(v[i]==0){
                 ans++;
-                dfs(him,vis,i);
+                solve(v,him,i);
             }
         }
         return ans;
     }
-    void dfs(List<List<Integer>> him,int []vis,int ind){
-        for(int i:him.get(ind)){
-            if(vis[i]==0){
-                vis[i]=1;
-                dfs(him,vis,i);
+    void solve(int v[],Map<Integer,List<Integer>> him,int i){
+        for(int j:him.get(i)){
+            if(v[j]==0){
+                v[j]=1;
+                solve(v,him,j);
             }
         }
     }
