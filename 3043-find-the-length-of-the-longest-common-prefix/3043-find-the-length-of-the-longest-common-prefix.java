e1fb1e3765b4e53a850169d02c914bd36d87add2
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-05-21 20:17:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -1,32 +1,33 @@
 class trie{
-trie(){
-    root=new Node();
-}
-       class Node{
-Node child[];
-boolean isEnd;
-Node (){
-    child=new Node[10];
-    isEnd=false;
-}
+    trie(){
+        root=new Node();
     }
-     Node root;
+    class Node{
+        Node child[];
+        boolean isEnd;
+        Node(){
+            child=new Node[10];
+            boolean isEnd=false;
+        }
+    }
+    Node root;
     void insert(String s){
         Node curr=root;
         for(int i=0;i<s.length();i++){
-            int ind=s.charAt(i)-'0';
-            if(curr.child[ind]==null){
-                curr.child[ind]=new Node();
+            int index=s.charAt(i)-'0';
+            if(curr.child[index]==null){
+                curr.child[index]=new Node();
             }
-            curr=curr.child[ind];
+            curr=curr.child[index];
         }
-      curr.isEnd=true;
+        curr.isEnd=true;
+
     }
     int prefix(String s){
-        int c=0;
         Node curr=root;
+        int c=0;
         for(int i=0;i<s.length();i++){
-             int ind=s.charAt(i)-'0';
+            int ind=s.charAt(i)-'0';
             if(curr.child[ind]==null){
                 break;
             }
@@ -35,21 +36,16 @@ Node (){
         }
         return c;
     }
-    
 }
-
-
 class Solution {
- 
-   
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        trie him=new trie();
+        trie t=new trie();
         for(int i:arr1){
-            him.insert(Integer.toString(i));
+            t.insert(Integer.toString(i));
         }
-        int ans=0;
-        for(int i:arr2){
-            ans=Math.max(ans,him.prefix(Integer.toString(i)));
+        int ans=Integer.MIN_VALUE;
+        for (int i:arr2){
+            ans=Math.max(ans,t.prefix(Integer.toString(i)));
         }
         return ans;
     }
