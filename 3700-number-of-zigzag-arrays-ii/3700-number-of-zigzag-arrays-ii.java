1027561aea1071fef472bc626e4c542659e43e13
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-06-24 11:17:14 +0530

    Time: 309 ms (58.06%), Space: 46.8 MB (61.29%) - LeetHub

---
+++ 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.java
@@ -0,0 +1,104 @@
+class Solution {
+    static final long MOD = 1000000007L;
+
+    public int zigZagArrays(int n, int l, int r) {
+        int m = r - l + 1;
+        int sz = 2 * m;
+
+        if (n == 1) {
+            return (int) (2L * m % MOD);
+        }
+
+        long[][] T = new long[sz][sz];
+
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < i; j++) {
+                T[i][m + j] = 1;
+            }
+
+            for (int j = i + 1; j < m; j++) {
+                T[m + i][j] = 1;
+            }
+        }
+
+        long[][] P = power(T, n - 1);
+
+        long[] base = new long[sz];
+        for (int i = 0; i < sz; i++) {
+            base[i] = 1;
+        }
+
+        long[] res = multiply(P, base);
+
+        long ans = 0;
+        for (long x : res) {
+            ans = (ans + x) % MOD;
+        }
+
+        return (int) ans;
+    }
+
+    private long[][] power(long[][] mat, int exp) {
+        int n = mat.length;
+
+        long[][] res = new long[n][n];
+        for (int i = 0; i < n; i++) {
+            res[i][i] = 1;
+        }
+
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                res = multiply(res, mat);
+            }
+
+            mat = multiply(mat, mat);
+            exp >>= 1;
+        }
+
+        return res;
+    }
+
+    private long[][] multiply(long[][] A, long[][] B) {
+        int n = A.length;
+        long[][] C = new long[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < n; k++) {
+
+                if (A[i][k] == 0) continue;
+
+                long a = A[i][k];
+
+                for (int j = 0; j < n; j++) {
+
+                    if (B[k][j] == 0) continue;
+
+                    C[i][j] = (C[i][j] + a * B[k][j]) % MOD;
+                }
+            }
+        }
+
+        return C;
+    }
+
+    private long[] multiply(long[][] A, long[] v) {
+        int n = A.length;
+        long[] res = new long[n];
+
+        for (int i = 0; i < n; i++) {
+
+            long sum = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (A[i][j] == 0) continue;
+
+                sum = (sum + A[i][j] * v[j]) % MOD;
+            }
+
+            res[i] = sum;
+        }
+
+        return res;
+    }
+}
