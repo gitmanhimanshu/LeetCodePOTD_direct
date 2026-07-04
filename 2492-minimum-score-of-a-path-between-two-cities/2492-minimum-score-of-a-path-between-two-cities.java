e74fffe52de40089c2e9486957187ae87394857b
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-07-04 11:02:30 +0530

    Time: 28 ms (59.87%), Space: 161.9 MB (68.64%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -9,17 +9,33 @@ class Solution {
             him.get(a[1]).add(new int[]{a[0],a[2]});
         }
         int v[]=new int[n+1];
-        return dfs(v,him,1);
-    }
-    int dfs(int v[],List<List<int[]>> him,int i){
-        v[i]=1;
+        // return dfs(v,him,1);
         int r=Integer.MAX_VALUE;
-        for(int a[]:him.get(i)){
-            r=Math.min(r,a[1]);
-            if(v[a[0]]==0){
-                r=Math.min(r,dfs(v,him,a[0]));
+        Queue<Integer>h=new LinkedList<>();
+        h.add(1);
+        while(!h.isEmpty()){
+            int ele=h.poll();
+            v[ele]=1;
+            for(int a[]:him.get(ele)){ 
+                r=Math.min(r,a[1]);
+                if(v[a[0]]==0){
+                    v[a[0]]=1;
+                   
+                    h.add(a[0]);
+                }
             }
         }
         return r;
     }
+    // int dfs(int v[],List<List<int[]>> him,int i){
+    //     v[i]=1;
+    //     int r=Integer.MAX_VALUE;
+    //     for(int a[]:him.get(i)){
+    //         r=Math.min(r,a[1]);
+    //         if(v[a[0]]==0){
+    //             r=Math.min(r,dfs(v,him,a[0]));
+    //         }
+    //     }
+    //     return r;
+    // }
 }
