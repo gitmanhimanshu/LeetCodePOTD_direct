d0f0d8ebebb15690fae9110b0e1ec56068877c82
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-08-02 13:20:20 +0530

    Time: 7 ms (21.79%), Space: 46.8 MB (13.54%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,13 +1,29 @@
 class Solution {
+    int dp[][];
     public boolean stoneGame(int[] piles) {
-        int evsum=0;
-        int odsum=0;
-        for(int i=0;i<piles.length;i+=2){
-            evsum+=piles[i];
+        int sum=0;
+        dp=new int[piles.length+1][piles.length+1];
+        for(int a[]:dp){
+            Arrays.fill(a,-1);
         }
-          for(int i=1;i<piles.length;i+=2){
-           odsum+=piles[i];
+        for(int i:piles){
+            sum+=i;
         }
-return evsum>odsum?true:true;
+        int aliceGot=solve(0,piles.length-1,piles);
+        int bobGot=sum-aliceGot;
+        return aliceGot>bobGot;
+
+        
+    }
+    int solve(int i,int j,int a[]){
+        if(i>j){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int take_i=a[i]+Math.min(solve(i+2,j,a),solve(i+1,j-1,a));
+        int take_j=a[j]+Math.min(solve(i+1,j-1,a),solve(i,j-2,a));
+        return dp[i][j]=Math.max(take_i,take_j);
     }
 }
