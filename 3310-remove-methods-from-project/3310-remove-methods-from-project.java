6108bf2813599ec61825ce1b6e428d22319cbb18
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-08-05 14:59:27 +0530

    Time: 61 ms (81.73%), Space: 289.8 MB (45.19%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.java
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -8,8 +8,7 @@ class Solution {
             him.get(a[0]).add(a[1]);
         }
         boolean sp[] = new boolean[n];
-        boolean vis[] = new boolean[n];
-        solve(sp, him, k, vis);
+        solve(sp, him, k);
         List<Integer> ans = new ArrayList<>();
         boolean got = false;
         for (int a[] : invocations) {
@@ -33,12 +32,12 @@ class Solution {
         return ans;
     }
 
-    void solve(boolean a[], List<List<Integer>> him, int i, boolean vis[]) {
+    void solve(boolean a[], List<List<Integer>> him, int i) {
         a[i] = true;
-        vis[i] = true;
+        
         for (int j : him.get(i)) {
-            if (!vis[j]) {
-                solve(a, him, j, vis);
+            if (!a[j]) {
+                solve(a, him, j);
             }
         }
     }
