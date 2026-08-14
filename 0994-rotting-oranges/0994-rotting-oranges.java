1cd1f71674567f25f62014bc7be9abccb4e0a3f0
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-08-14 12:46:40 +0530

    Time: 2 ms (89.67%), Space: 44.1 MB (72.57%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,63 +1,60 @@
+class Pair{
+    int i;
+    int j;
+    public Pair(int i,int j){
+        this.i=i;
+        this.j=j;
+    }
+}
 class Solution {
-
-    boolean isValid(int [][]a, int x, int y){
-        return x>=0 && y>=0 && x<a.length && y<a[0].length && a[x][y]==1;
-    }
-
-    class Pair{
-        int i, j;
-        Pair(int i, int j){
-            this.i = i;
-            this.j = j;
-        }
-    }
-
+    int n;
     public int orangesRotting(int[][] grid) {
-
-        Queue<Pair> q = new LinkedList<>();
-
-        // add all rotten oranges
+        n=grid.length;
+        int one=0;
+        Queue<Pair> him=new LinkedList<>();
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==2){
-                    q.add(new Pair(i,j));
+                    him.add(new Pair(i,j));
+                }else if(grid[i][j]==1){
+                    one++;
                 }
             }
         }
+        if(one==0){
+            return 0;
+        }
 
-        int time = 0;
-        int[] x = {1,-1,0,0};
-        int[] y = {0,0,1,-1};
-
-        while(!q.isEmpty()){
-            int size = q.size();
-            boolean rotted = false;
-
-            for(int i=0;i<size;i++){
-                Pair p = q.poll();
-
-                for(int d=0; d<4; d++){
-                    int nx = p.i + x[d];
-                    int ny = p.j + y[d];
-
-                    if(isValid(grid, nx, ny)){
-                        grid[nx][ny] = 2;
-                        q.add(new Pair(nx, ny));
-                        rotted = true;
+        int ans=0;
+        int dir[][]={{1,0},{0,1},{-1,0},{0,-1}};
+        while(!him.isEmpty()){
+            int l=him.size();
+            for(int k=1;k<=l;k++){
+                Pair p=him.poll();
+                int i=p.i;
+                int j=p.j;
+                for(int d[]:dir){
+                    int new_i=i+d[0];
+                    int new_j=j+d[1];
+                    if(isValid(new_i,new_j,grid)){
+                        him.add(new Pair(new_i,new_j));
+                        grid[new_i][new_j]=2;
+                        one--;
                     }
                 }
+               
             }
-
-            if(rotted) time++;
+             ans++;
         }
-
-        // check if any fresh orange left
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1) return -1;
-            }
+        if(one==0){
+            return ans-1;
         }
-
-        return time;
+        return -1;
+    }
+    boolean isValid(int i,int j,int [][]a){
+        if(i>=0&&i<n&&j>=0&&j<a[0].length&&a[i][j]==1){
+            return true;
+        }
+        return false;
     }
 }
