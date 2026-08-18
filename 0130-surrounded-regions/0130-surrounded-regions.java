f571c505b5db64e8f37635cdb7a9c7e09e0d07b7
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-08-18 13:09:35 +0530

    Time: 4 ms (15.74%), Space: 47.9 MB (39.15%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -16,22 +16,24 @@ class Solution {
 }
     public void solve(char[][] board) {
         Queue<Pair> him = new LinkedList<>();
-        Set<String> st = new HashSet<>();
+        
         for (int i = 0; i < board.length; i++) {
             if (board[i][0] == 'O') {
                 him.add(new Pair(i, 0));
-                st.add(i + "," + 0);
+                board[i][0]='#';
+               
             }
             if (board[i][board[0].length - 1] == 'O') {
                 him.add(new Pair(i, board[0].length - 1));
-                st.add(i + "," + (board[0].length - 1));
+               board[i][board[0].length - 1]='#';
             }
         }
 
         for (int j = 0; j < board[0].length; j++) {
             if (board[0][j] == 'O') {
                 him.add(new Pair(0, j));
-                st.add(0 + "," + j);
+                 board[0][j]='#';
+               
             }
             // if(j==1){
             //     System.out.println(board[board.length - 1][j]);
@@ -40,18 +42,19 @@ class Solution {
             if (board[board.length - 1][j]=='O') {
                 // System.out.println(board[board.length - 1][j]);
                 him.add(new Pair(board.length - 1, j));
-                st.add((board.length - 1) + "," + j);
+                 board[board.length - 1][j]='#';
+               
             }
         }
             int dir[][]=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-            System.out.println(st);
+          
         while(!him.isEmpty()){
             Pair p=him.poll();
             for(int d[]:dir){
                 int next_i=p.i+d[0];
                 int next_j=p.j+d[1];
-                if(isValid(next_i,next_j,board)&&!st.contains(next_i+","+next_j)){
-                    st.add(next_i+","+next_j);
+                if(isValid(next_i,next_j,board)){
+                    board[next_i][next_j]='#';
                     him.add(new Pair(next_i,next_j));
                 }
             }
@@ -59,12 +62,20 @@ class Solution {
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 if(board[i][j]=='O'){
-                    if(!st.contains(i+","+j)){
+                    
                         board[i][j]='X';
-                    }
+                    
                 }
             }
         }
-        
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]=='#'){
+                    
+                        board[i][j]='O';
+                    
+                }
+            }
+        }
     }
 }
