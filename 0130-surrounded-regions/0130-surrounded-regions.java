ea3cf54cbf5aeae5606a4007f6f83cb14858431f
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-08-18 13:10:05 +0530

    Time: 3 ms (27.72%), Space: 47.8 MB (39.15%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -65,17 +65,13 @@ class Solution {
                     
                         board[i][j]='X';
                     
-                }
-            }
-        }
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]=='#'){
+                }else  if(board[i][j]=='#'){
                     
                         board[i][j]='O';
                     
                 }
             }
         }
+
     }
 }
