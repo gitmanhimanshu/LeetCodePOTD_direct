20fa1c65b7f6bd745332186089a6a470e3c7bb55
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-08-19 12:13:17 +0530

    Time: 100 ms (25.21%), Space: 49.5 MB (17.88%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<String> him=new LinkedList<>();
+        Set<String> vis=new HashSet<>();
+        int l=0;
+        if(!wordList.contains(endWord)){
+            return l;
+        }
+        Set<String> h=new HashSet<>(wordList);
+        him.add(beginWord);
+        while(!him.isEmpty()){
+            int n=him.size();
+            l++;
+            for(int i=1;i<=n;i++){
+                String t=him.poll();
+                for(int j=0;j<t.length();j++){
+                    char c[]=t.toCharArray();
+                    for(char k='a';k<='z';k++){
+                        c[j]=k;
+                    String new1=new String(c);
+                    if(new1.equals(endWord)){
+                        return l+1;
+                    }
+                    if(!vis.contains(new1)&&h.contains(new1)){
+                        him.add(new1);
+                        vis.add(new1);
+                    }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
