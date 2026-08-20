2eba90b66977efaef33e6b94321549487ceaa0c3
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-08-20 12:23:09 +0530

    Time: 0 ms (100%), Space: 47.1 MB (77.5%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,38 +1,24 @@
-import java.util.*;
-
 class Solution {
-    private static final int RED = 0;
-    private static final int BLUE = 1;
-
     public boolean isBipartite(int[][] graph) {
-        int[] check = new int[graph.length];
-        Map<Integer, Set<Integer>> adjList = new HashMap<>();
-        
-        for (int i = 0; i < graph.length; i++) {
-            adjList.put(i, new HashSet<>());
-            for (int j : graph[i]) {
-                adjList.get(i).add(j);
-            }
-        }
-        
-        Arrays.fill(check, -1);
-        
-        for (int i = 0; i < check.length; i++) {
-            if (check[i] == -1 && !dfsCheck(check, i, adjList, RED)) {
-                return false;
+        int color[]=new int[graph.length];
+        Arrays.fill(color,-1);
+        for(int i=0;i<color.length;i++){
+            if(color[i]==-1){
+                if(!solve(graph,0,i,color)){
+                    return false;
+                }
             }
         }
         return true;
     }
-    
-    private boolean dfsCheck(int[] check, int curr, Map<Integer, Set<Integer>> adjList, int color) {
-        check[curr] = color;
-        
-        for (int neighbor : adjList.getOrDefault(curr, new HashSet<>())) {
-            if (check[neighbor] == color) {
-                return false;
-            }
-            if (check[neighbor] == -1 && !dfsCheck(check, neighbor, adjList, 1 - color)) {
+    boolean solve(int graph[][],int rang,int i,int color[]){
+        color[i]=rang;
+        for(int j:graph[i]){
+            if(color[j]==-1){
+                if(!solve(graph,1-rang,j,color)){
+                    return false;
+                }
+            }else if(color[i]==color[j]){
                 return false;
             }
         }
