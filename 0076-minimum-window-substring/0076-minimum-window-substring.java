dcbb9825139baa9a1b3a2b951b5989d1e6c34929
Himanshu Yadav <165804206+gitmanhimanshu@users.noreply.github.com>
2026-09-18 14:35:09 +0530

    Time: 14 ms (58.61%), Space: 47 MB (50.86%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,41 +1,40 @@
 class Solution {
     public String minWindow(String s, String t) {
         Map<Character,Integer> him=new HashMap<>();
-      
-    for(char i:t.toCharArray()){
-        him.put(i,him.getOrDefault(i,0)+1);
-    }
-      int st=-1;
-        int i=0;
-        int r=0;
-        int c=0;
-        int ml=Integer.MAX_VALUE;
-        while(r<s.length()){
-            if(him.containsKey(s.charAt(r))){
-                if(him.get(s.charAt(r))>0){
-                    c++;
-                }
-            }
-             him.put(s.charAt(r),him.getOrDefault(s.charAt(r),0)-1); 
-             while(c==t.length()){
-                if((r-i+1)<ml){
-                    st=i;
-                    ml=r-i+1;
-                }
-                him.put(s.charAt(i),him.getOrDefault(s.charAt(i),0)+1); 
-                if(him.get(s.charAt(i))>0){
-                    c--;
-                }
-                i++;
-               if(i>=s.length()){
-                break;
-               }
-             }
-             r++;
-        }
-        if(st==-1){
+        if(t.length()>s.length()){
             return "";
         }
-        return s.substring(st,st+ml);
+        for(char c:t.toCharArray()){
+            him.put(c,him.getOrDefault(c,0)+1);
+        }
+        int required=t.length();
+        int i=0;
+        int j=0;
+        int start=i;
+        int ans=Integer.MAX_VALUE;
+
+        while(j<s.length()){
+            char c=s.charAt(j);
+            if(him.getOrDefault(c,0)>0){
+                required--;
+            }
+            him.put(c,him.getOrDefault(c,0)-1);
+            while(required==0){
+                int length=j-i+1;
+                if(length<ans){
+                    ans=length;
+                    start=i;
+                }
+                char b=s.charAt(i);
+                him.put(b,him.getOrDefault(b,0)+1);
+                if(him.get(b)>0){
+                    required++;
+                }
+                i++;
+            }
+            j++;
+            
+        }
+        return ans==Integer.MAX_VALUE?"":s.substring(start,start+ans);
     }
 }
